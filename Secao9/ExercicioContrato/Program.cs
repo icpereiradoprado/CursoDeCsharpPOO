9333d1a4617f6fe07c14d1292c4686389aafe404
Isaac <icpereiradoprado@gmail.com>
2022-04-08 16:01:14 -0300

    Atualização Seção 9

--- Secao9/ExercicioContrato/Program.cs
+++ Secao9/ExercicioContrato/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using ExercicioContrato.Entities;
+using ExercicioContrato.Entities.Enums;
+
+namespace ExercicioContrato
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter departament's name: ");
+            string nameDepartament = Console.ReadLine();
+            
+            Console.WriteLine("\n--- Enter worker data ---\n");
+
+            Console.Write("Name: ");
+            string nameWoroker = Console.ReadLine();
+            Console.Write("Level (Junior|MidLevel|Senior: )");
+            WorkerLevel workLevel = Enum.Parse<WorkerLevel>(Console.ReadLine());
+            Console.Write("Base Salary: ");
+            double baseSalary = double.Parse(Console.ReadLine());
+            Console.Write("How many contracts to this worker? ");
+            int quantityContracts = int.Parse(Console.ReadLine());
+        }
+    }
+}
