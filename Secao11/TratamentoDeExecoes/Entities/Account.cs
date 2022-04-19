91612c6e3d344e6fa05eeaf0da50d9989453b2d2
Isaac Prado <icpereiradoprado@gmail.com>
2022-04-19 13:36:44 -0300

    Add section 11

---
+++ Secao11/TratamentoDeExecoes/Entities/Account.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using TratamentoDeExecoes.Entities.Exceptions;
+
+namespace TratamentoDeExecoes.Entities
+{
+    class Account
+    {
+        public int Number { get; set; }
+        public string Holder { get; set; }
+        public double Balance { get; private set; }
+        public double WithDrawLimit{ get; set; }
+
+        public Account()
+        {
+
+        }
+
+        public Account(int number, string holder, double withDrawLimit)
+        {
+            Number = number;
+            Holder = holder;
+            WithDrawLimit = withDrawLimit;
+        }
+
+        //demais métodos
+
+        public void Deposit(double amount)
+        {
+            Balance = amount;
+        }
+
+        public void WithDraw(double amount)
+        {
+            
+            if(Balance < 0 || Balance < amount)
+            {
+                throw new DomainException("Not enough balance");
+            }
+            if(amount > WithDrawLimit)
+            {
+                throw new DomainException("The amount exceds withdraw limit");
+            }
+            Balance -= amount;
+        }
+    }
+}
---
+++ Secao11/TratamentoDeExecoes/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using TratamentoDeExecoes.Entities.Exceptions;
+using TratamentoDeExecoes.Entities;
+
+namespace TratamentoDeExecoes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Enter account data: ");
+                Console.Write("Number: ");
+                int accountNumber = int.Parse(Console.ReadLine());
+                Console.Write("Holder: ");
+                string holder = Console.ReadLine();
+                Console.Write("Initial balance: ");
+                double balance = double.Parse(Console.ReadLine());
+                Console.Write("Withdraw Limit: ");
+                double withDrawLimit = double.Parse(Console.ReadLine());
+
+
+                Account account = new Account(accountNumber, holder, withDrawLimit);
+                account.Deposit(balance);
+
+                Console.Write("\n\nEnter amount for withdraw: ");
+                double ammount = double.Parse(Console.ReadLine());
+                account.WithDraw(ammount);
+
+                Console.WriteLine($"New Balance: {account.Balance:F2}");
+            }
+            catch (DomainException e)
+            {
+                Console.WriteLine($"Withdraw error: {e.Message}");
+            }
+
+
+        }
+    }
+}
